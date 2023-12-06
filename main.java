82aa88fe33bac861294f0c5d7831d34d0c887615
Gordon Li <121423020+gor-don-li@users.noreply.github.com>
2023-12-05 20:50:48 -0800

    Update main.java

--- main.java
+++ main.java
@@ -2,21 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         int n = 10; // Change this value to generate a different number of Fibonacci numbers
-        int[] fibonacci = new int[n];
+        int[] sequence = new int[n];
 
-        fibonacci[0] = 0; // First Fibonacci number
-        fibonacci[1] = 1; // Second Fibonacci number
+        sequence[0] = 0; // First Fibonacci number
+        sequence[1] = 1; // Second Fibonacci number
 
         /* Initializing the first two numbers in the Fibonacci sequence is necessary
          * because the sequence is defined by the sum of the two preceding numbers. */
 
         for (int i = 2; i < n; i++) {
-            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
         }
 
         System.out.println("Fibonacci Sequence:");
         for (int i = 0; i < n; i++) {
-            System.out.print(fibonacci[i] + " ");
+            System.out.print(sequence[i] + " ");
         }
     }
 }
